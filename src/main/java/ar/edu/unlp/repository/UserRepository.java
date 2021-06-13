28cb85c3989a7729b10ff08bc1b79cf1799c67d6
Nicolas Castro <pcastro@unrn.edu.ar>
2021-06-13 18:26:01 -0300

    Remove unused imports

--- src/main/java/ar/edu/unlp/repository/UserRepository.java
+++ src/main/java/ar/edu/unlp/repository/UserRepository.java
@@ -3,9 +3,7 @@ package ar.edu.unlp.repository;
 import ar.edu.unlp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, String> {
 
     User findByUsername(String anUsername);
 
